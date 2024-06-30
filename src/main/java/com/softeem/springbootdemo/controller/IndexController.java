9150c772df95b0debd47a51a50e649876bfca165
YIngWi <lqbyuuu@gmail.com>
2024-06-30 11:07:49 +0800

    asdfalskdjflasdf a

---
+++ src/main/java/com/softeem/springbootdemo/controller/IndexController.java
@@ -0,0 +1,10 @@
+package com.softeem.springbootdemo.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class IndexController {
+    @RequestMapping("/")
+    public String index(){
+        return "这是一个首页";
+    }
+}
